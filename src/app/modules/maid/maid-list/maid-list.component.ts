019784dd68ffb2de44e248101598e85d25c7bd3f
flame123flame <flame.tana@gmail.com>
2023-09-10 19:41:59 +0700

    add

--- src/app/modules/maid/maid-list/maid-list.component.ts
+++ src/app/modules/maid/maid-list/maid-list.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+interface Person {
+  key: string;
+  name: string;
+  age: number;
+  address: string;
+}
 
 @Component({
   selector: 'app-maid-list',
@@ -6,5 +12,114 @@ import { Component } from '@angular/core';
   styleUrls: ['./maid-list.component.scss']
 })
 export class MaidListComponent {
-
+  listOfData: Person[] = [
+    {
+      key: '1',
+      name: 'John Brown',
+      age: 32,
+      address: 'New York No. 1 Lake Park'
+    },
+    {
+      key: '2',
+      name: 'Jim Green',
+      age: 42,
+      address: 'London No. 1 Lake Park'
+    },
+    {
+      key: '3',
+      name: 'Joe Black',
+      age: 32,
+      address: 'Sidney No. 1 Lake Park'
+    },
+    {
+      key: '1',
+      name: 'John Brown',
+      age: 32,
+      address: 'New York No. 1 Lake Park'
+    },
+    {
+      key: '2',
+      name: 'Jim Green',
+      age: 42,
+      address: 'London No. 1 Lake Park'
+    },
+    {
+      key: '3',
+      name: 'Joe Black',
+      age: 32,
+      address: 'Sidney No. 1 Lake Park'
+    },
+    {
+      key: '1',
+      name: 'John Brown',
+      age: 32,
+      address: 'New York No. 1 Lake Park'
+    },
+    {
+      key: '2',
+      name: 'Jim Green',
+      age: 42,
+      address: 'London No. 1 Lake Park'
+    },
+    {
+      key: '3',
+      name: 'Joe Black',
+      age: 32,
+      address: 'Sidney No. 1 Lake Park'
+    },
+    {
+      key: '1',
+      name: 'John Brown',
+      age: 32,
+      address: 'New York No. 1 Lake Park'
+    },
+    {
+      key: '2',
+      name: 'Jim Green',
+      age: 42,
+      address: 'London No. 1 Lake Park'
+    },
+    {
+      key: '3',
+      name: 'Joe Black',
+      age: 32,
+      address: 'Sidney No. 1 Lake Park'
+    },
+    {
+      key: '1',
+      name: 'John Brown',
+      age: 32,
+      address: 'New York No. 1 Lake Park'
+    },
+    {
+      key: '2',
+      name: 'Jim Green',
+      age: 42,
+      address: 'London No. 1 Lake Park'
+    },
+    {
+      key: '3',
+      name: 'Joe Black',
+      age: 32,
+      address: 'Sidney No. 1 Lake Park'
+    },
+    {
+      key: '1',
+      name: 'John Brown',
+      age: 32,
+      address: 'New York No. 1 Lake Park'
+    },
+    {
+      key: '2',
+      name: 'Jim Green',
+      age: 42,
+      address: 'London No. 1 Lake Park'
+    },
+    {
+      key: '3',
+      name: 'Joe Black',
+      age: 32,
+      address: 'Sidney No. 1 Lake Park'
+    }
+  ];
 }
--- src/app/modules/maid/maid.module.ts
+++ src/app/modules/maid/maid.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { DashboardComponent } from 'src/app/modules/dashboard/dashboard.component';
 import { MaidListComponent } from './maid-list/maid-list.component';
 import { MaidRoutingModule } from './maid-routing.module';
-
+import { NzTableModule } from 'ng-zorro-antd/table';
 @NgModule({
   declarations: [
     MaidListComponent,
   ],
   imports: [
     CommonModule,
+    NzTableModule,
     MaidRoutingModule,
   ]
 })
